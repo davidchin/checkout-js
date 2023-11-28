3b3e5c12d85212e7a7c6ffa2aa4eb16b308d8b5f
David Chin <davidchin.yf@gmail.com>
2023-11-28 16:08:18 +1100

    fix(common): CHECKOUT-6524 Fix Sentry client initialization to avoid conflict

--- packages/core/src/app/common/error/SentryErrorLogger.ts
+++ packages/core/src/app/common/error/SentryErrorLogger.ts
@@ -1,13 +1,17 @@
 import {
+    BrowserClient,
     BrowserOptions,
-    captureException,
+    defaultIntegrations,
+    defaultStackParser,
     Event,
-    init,
+    Hub,
     Integrations,
+    makeFetchTransport,
+    makeXHRTransport,
     SeverityLevel,
     StackFrame,
-    withScope,
 } from '@sentry/browser';
+import { BrowserClientOptions } from '@sentry/browser/types/client';
 import { RewriteFrames } from '@sentry/integrations';
 import { EventHint, Exception } from '@sentry/types';
 
@@ -29,6 +33,7 @@ export interface SentryErrorLoggerOptions {
     errorTypes?: string[];
     publicPath?: string;
     sampleRate?: number;
+    createHub?: (options: BrowserClientOptions) => Hub;
 }
 
 export enum SeverityLevelEnum {
@@ -41,10 +46,12 @@ export enum SeverityLevelEnum {
 export default class SentryErrorLogger implements ErrorLogger {
     private consoleLogger: ErrorLogger;
     private publicPath: string;
+    private hub: Hub;
 
     constructor(config: BrowserOptions, options?: SentryErrorLoggerOptions) {
         const {
             consoleLogger = new NoopErrorLogger(),
+            createHub = (options: BrowserClientOptions) => new Hub(new BrowserClient(options)),
             publicPath = '',
             sampleRate = 0.1,
         } = options || {};
@@ -52,7 +59,7 @@ export default class SentryErrorLogger implements ErrorLogger {
         this.consoleLogger = consoleLogger;
         this.publicPath = publicPath;
 
-        init({
+        const clientOptions = {
             sampleRate,
             beforeSend: this.handleBeforeSend,
             denyUrls: [
@@ -61,6 +68,10 @@ export default class SentryErrorLogger implements ErrorLogger {
                 'sentry~checkout',
             ],
             integrations: [
+                ...defaultIntegrations.filter(integration =>
+                    integration.name !== 'GlobalHandlers' &&
+                    integration.name !== 'RewriteFrames'
+                ),
                 new Integrations.GlobalHandlers({
                     onerror: false,
                     onunhandledrejection: true,
@@ -69,8 +80,12 @@ export default class SentryErrorLogger implements ErrorLogger {
                     iteratee: this.handleRewriteFrame,
                 }),
             ],
+            transport: 'fetch' in window ? makeFetchTransport : makeXHRTransport,
+            stackParser: defaultStackParser,
             ...config,
-        });
+        };
+
+        this.hub = createHub(clientOptions as BrowserClientOptions);
     }
 
     log(
@@ -81,7 +96,7 @@ export default class SentryErrorLogger implements ErrorLogger {
     ): void {
         this.consoleLogger.log(error, tags, level);
 
-        withScope((scope) => {
+        this.hub.withScope((scope) => {
             const { errorCode = computeErrorCode(error) } = tags || {};
 
             if (errorCode) {
@@ -96,7 +111,7 @@ export default class SentryErrorLogger implements ErrorLogger {
 
             scope.setFingerprint(['{{ default }}']);
 
-            captureException(error);
+            this.hub.captureException(error);
         });
     }
 
